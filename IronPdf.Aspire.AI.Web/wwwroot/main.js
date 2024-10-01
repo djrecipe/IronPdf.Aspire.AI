fdd74b8e598dc678453b72ae334e9f9fdb22ccd5
paing-ironsoftware <paing@ironsoftware.com>
2024-10-01 15:05:13 +0700

    Added drag and drop ability

---
+++ IronPdf.Aspire.AI.Web/wwwroot/main.js
@@ -0,0 +1,17 @@
+﻿export function initialize(drop_area) {
+    drop_area.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+
+    drop_area.addEventListener('drop', (e) => {
+        e.preventDefault();
+
+        var hiddenInputFile = document.querySelector('.hidden-input-file');
+
+        hiddenInputFile.files = e.dataTransfer.files;
+
+        var customChangeEvent = new Event('change', { bubbles: true });
+
+        hiddenInputFile.dispatchEvent(customChangeEvent);
+    });
+}
